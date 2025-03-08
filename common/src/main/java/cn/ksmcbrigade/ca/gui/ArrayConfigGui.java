299f652fa74b1a3d0add4021d19310e555a1b8f1
yiyuyan <yiyuyan>
2025-03-08 08:42:03 +0800

    UPDATE 1.1.3 VERSION

--- common/src/main/java/cn/ksmcbrigade/ca/gui/ArrayConfigGui.java
+++ common/src/main/java/cn/ksmcbrigade/ca/gui/ArrayConfigGui.java
@@ -1,7 +1,11 @@
 package cn.ksmcbrigade.ca.gui;
 
 import cn.ksmcbrigade.ca.config.Config;
+import cn.ksmcbrigade.ca.config.Configs;
+import cn.ksmcbrigade.ca.gui.entry.ArrStringGui;
 import cn.ksmcbrigade.ca.gui.list.ArrayConfigList;
+import cn.ksmcbrigade.ca.gui.list.entrys.ArrStringEntry;
+import cn.ksmcbrigade.ca.gui.list.entrys.Entry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -11,6 +15,8 @@ import net.minecraft.client.gui.screens.options.OptionsSubScreen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public class ArrayConfigGui extends OptionsSubScreen {
     public final Config config;
@@ -39,10 +45,50 @@ public class ArrayConfigGui extends OptionsSubScreen {
 
     protected void addFooter() {
         LinearLayout linearlayout = this.layout.addToFooter(LinearLayout.horizontal().spacing(8));
-        linearlayout.addChild(Button.builder(CommonComponents.GUI_DONE, (p_344249_) -> this.onClose()).build());
+        linearlayout.addChild(Button.builder(Component.literal("Add"),(b)->{
+            ArrayList<String> arr = new ArrayList<>(this.config.getArray(this.key));
+            int i = arr.size();
+            arr.add("");
+            this.config.put(this.key,arr);
+            Minecraft.getInstance().setScreen(new ArrStringGui(new ArrayConfigGui(this.lastScreen,this.config,this.key),this.config,this.key,i));
+        }).width(100).build());
+        linearlayout.addChild(Button.builder(Component.literal("Remove"),(b)->{
+            if(this.list!=null){
+                for (Entry child : this.list.children()) {
+                    if(child instanceof ArrStringEntry arrStringEntry){
+                        if(arrStringEntry.isFocused()){
+                            remove(this.config,this.key,arrStringEntry.getContext());
+                        }
+                    }
+                }
+                Minecraft.getInstance().setScreen(new ArrayConfigGui(this.lastScreen,this.config,this.key));
+            }
+        }).width(100).build());
+        linearlayout.addChild(Button.builder(CommonComponents.GUI_DONE, (p_344249_) -> this.onClose()).width(100).build());
     }
 
     public void render(@NotNull GuiGraphics p_344555_, int p_344302_, int p_344298_, float p_344857_) {
         super.render(p_344555_, p_344302_, p_344298_, p_344857_);
+    }
+
+    public static void add(Config config,String key,String context){
+        ArrayList<String> arr = new ArrayList<>(config.getArray(key));
+        arr.add(context);
+        config.put(key,arr);
+    }
+
+    public static void remove(Config config,String key,String context){
+        ArrayList<String> arr = new ArrayList<>(config.getArray(key));
+        arr.remove(context);
+        config.put(key,arr);
+    }
+
+    public static boolean replace(Config config,String key,String context,String target){
+        ArrayList<String> arr = new ArrayList<>(config.getArray(key));
+        int i = arr.indexOf(context);
+        if(i==-1) return false;
+        arr.set(i,target);
+        config.put(key,arr);
+        return true;
     }
 }
--- common/src/main/java/cn/ksmcbrigade/ca/gui/list/entrys/ArrStringEntry.java
+++ common/src/main/java/cn/ksmcbrigade/ca/gui/list/entrys/ArrStringEntry.java
@@ -3,6 +3,7 @@ package cn.ksmcbrigade.ca.gui.list.entrys;
 import cn.ksmcbrigade.ca.config.Config;
 import cn.ksmcbrigade.ca.gui.entry.ArrStringGui;
 import cn.ksmcbrigade.ca.gui.list.ArrayConfigList;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -10,7 +11,9 @@ import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.glfw.GLFW;
 
+import java.awt.*;
 import java.util.List;
 
 public class ArrStringEntry extends Entry{
@@ -27,7 +30,8 @@ public class ArrStringEntry extends Entry{
         this.key = key;
         this.keyKey = keyKey;
         this.box = Button.builder(Component.literal("Edit"),(button -> {
-            Minecraft.getInstance().setScreen(new ArrStringGui(this.instance.last,this.config,this.key,keyKey));
+            if(!InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(),InputConstants.KEY_LCONTROL) && !InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(),InputConstants.KEY_RCONTROL))
+                Minecraft.getInstance().setScreen(new ArrStringGui(this.instance.last,this.config,this.key,keyKey));
         })).size(50,20).build();
     }
 
@@ -48,6 +52,13 @@ public class ArrStringEntry extends Entry{
         this.box.setPosition(i, j);
         this.box.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
         pGuiGraphics.drawString(Minecraft.getInstance().font, this.key+"#"+(this.keyKey+1), pLeft, pTop + pHeight / 2 - 4, -1);
+        if(this.isFocused()){
+            pGuiGraphics.fill(pLeft-1, pTop + pHeight / 2,pLeft, pTop + pHeight / 2+1, Color.RED.getRGB());
+        }
+    }
+
+    public String getContext(){
+        return this.config.getArray(this.key).get(this.keyKey);
     }
 
     @Override
